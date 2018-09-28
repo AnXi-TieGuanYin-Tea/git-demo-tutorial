e17089f28dd077f712b1f9f5fcdcb1f5bfcfea20
CooleyYang <cooleyyang@tencent.com>
2018-09-28 17:46:59 +0800

    Update hello.c

---
+++ hello.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(int argc, char **argv)
+{
+    int mIntVar1;
+    int *mPonit;
+
+    mIntVar1 = 100;
+    mPonit = &mIntVar1;
+
+    printf("mIntVar1 = %d, mPonit = %d\n", mIntVar1, *mPonit);
+    printf("Add At Develop Branch mIntVar1 = %d, mPonit = %d\n", mIntVar1, *mPonit);
+
+    exit(0);
+}
